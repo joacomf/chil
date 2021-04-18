d60adb062529c9add3369ead13a0bd67bbf83b71
Joaco Moreno <joaquin.moreno@grupoesfera.com.ar>
2021-04-18 18:56:24 -0300

    Reidenta archivo de PlataformaArduino

--- plataformas/arduino/src/PlataformaArduino.cpp
+++ plataformas/arduino/src/PlataformaArduino.cpp
@@ -1,14 +1,14 @@
 #include "PlataformaArduino.h"
 
-PlataformaArduino::PlataformaArduino(){
+PlataformaArduino::PlataformaArduino() {
     Serial.begin(PlataformaArduino::velocidadSerial);
 }
 
-void PlataformaArduino::escribir(int pin, int valor){
+void PlataformaArduino::escribir(int pin, int valor) {
     digitalWrite(pin, valor);
 }
 
-int PlataformaArduino::leer(int pin){
+int PlataformaArduino::leer(int pin) {
     return digitalRead(pin);
 }
 
@@ -16,11 +16,11 @@ unsigned long PlataformaArduino::milisegundos() {
     return millis();
 }
 
-unsigned long PlataformaArduino::microsegundos(){
+unsigned long PlataformaArduino::microsegundos() {
     return micros();
 }
 
-void PlataformaArduino::demorar(int milisegundos){
+void PlataformaArduino::demorar(int milisegundos) {
     delay(milisegundos);
 }
 
@@ -33,13 +33,13 @@ void PlataformaArduino::consola(const char *texto) {
 }
 
 bool PlataformaArduino::crearRedWiFi(const char *nombre, const char *clave) {
-  WiFi.enableAP(true);
-  this->apEncendido = WiFi.softAP(nombre, clave);
-  return this->apEncendido;
+    WiFi.enableAP(true);
+    this->apEncendido = WiFi.softAP(nombre, clave);
+    return this->apEncendido;
 }
 
 bool PlataformaArduino::estaAPEncendido() {
-  return this->apEncendido;
+    return this->apEncendido;
 }
 
 bool PlataformaArduino::apagarWiFi() {
@@ -50,7 +50,7 @@ bool PlataformaArduino::apagarWiFi() {
 void PlataformaArduino::crearServidorWeb() {
     this->servidor = new AsyncWebServer(80);
 
-    this->servidor->on("/chil-ping", HTTP_GET, [](AsyncWebServerRequest *request){
+    this->servidor->on("/chil-ping", HTTP_GET, [](AsyncWebServerRequest *request) {
         request->send(200, "text/plain", "chil-pong");
     });
 
