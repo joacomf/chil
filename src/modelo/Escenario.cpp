a9be7391de891a6dd2f7f2b1420f883066a3fe1d
Joaco Moreno <joaquin.moreno@grupoesfera.com.ar>
2021-05-02 21:10:23 -0300

    Documenta la clase Escenario

--- src/modelo/Escenario.cpp
+++ src/modelo/Escenario.cpp
@@ -1,13 +1,30 @@
 #include <modelo/Escenario.h>
 
+/**
+ * Clase para definir un escenario nuevo
+ *
+ * @class Escenario
+ *
+ * @param const char* nombre - Nombre a otorgar al escenario
+ */
 Escenario::Escenario(const char *nombre) {
   this->nombre = nombre;
 }
 
+/**
+ * Registra un paso definido al escenario en cuestion
+ *
+ * @param Paso* paso - referencia del paso a registrar en el escenario actual
+ */
 void Escenario::nuevo(Paso *paso) {
   this->_pasos.push_back(*paso);
 }
 
+/**
+ * Genera el reporte del escenario con el reporte de cada uno de sus pasos
+ *
+ * @return [out][string] resultado final formateado para mostrar por consola.
+ */
 string Escenario::imprimirResultado() {
   list<Paso>::iterator paso;
   string reporteDeEscenario = string("Escenario: " + string(this->nombre));
@@ -21,10 +38,20 @@ string Escenario::imprimirResultado() {
   return reporteDeEscenario;
 }
 
+/**
+ * Metodo para obtener la cantidad de pasos contenidos en el escenario
+ *
+ * @return [out][int] cantidad de pasos del escenario.
+ */
 int Escenario::obtenerCantidadPasos() {
   return this->_pasos.size();
 }
 
+/**
+ * Comanda finalizar la construccion del escenario y asi ejecutar cada paso contenido tomando los tiempos de ejecuciom
+ *
+ * @param Plataforma* framework - referencia a la plataforma para poder usar comandos propios de la misma
+ */
 void Escenario::finalizar(Plataforma *framework) {
   list<Paso>::iterator paso;
 
