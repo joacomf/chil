406815322f11a2a4f98b53fadd2f176bc3b47cd8
Joaco Moreno <joaquin.moreno@grupoesfera.com.ar>
2021-05-03 19:19:35 -0300

    Cambia carpeta generada para mostrar el html

--- scripts/generar-documentacion.sh
+++ scripts/generar-documentacion.sh
@@ -6,3 +6,4 @@ echo 'Generando documentación con Doxygen'
 set -e
 doxygen ./documentacion/configuracion
 cp ./documentacion/.nojekyll ./documentacion/html
+mv ./documentacion/html ./documentacion/docs
