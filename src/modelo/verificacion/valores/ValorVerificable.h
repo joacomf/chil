79c8f342beaf25067f1921f4f33122c1782bc959
Joaco Moreno <joaquin.moreno@grupoesfera.com.ar>
2022-01-23 02:03:55 -0300

    Mueve definición de template a archivo de cabecera para evitar problemas de linker

--- src/modelo/verificacion/valores/ValorVerificable.h
+++ src/modelo/verificacion/valores/ValorVerificable.h
@@ -23,7 +23,27 @@ private:
     Estrategia<Tipo>* estrategia{};
 };
 
-#include "ValorVerificable.cpp"
+template<typename Tipo>
+ValorVerificable<Tipo>::ValorVerificable(Tipo elValorAVerificar) {
+    this->valorAVerificar = elValorAVerificar;
+    this->estrategia = new EstrategiaDirectaConValores<Tipo>();
+}
+
+template <typename Tipo>
+void ValorVerificable<Tipo>::esIgualA(Tipo elValorEsperado) {
+    this->valorEsperado = elValorEsperado;
+    this->estrategia->verificar(this);
+}
+
+template <typename Tipo>
+Tipo ValorVerificable<Tipo>::obtenerValorEsperado() {
+    return this->valorEsperado;
+}
+
+template <typename Tipo>
+Tipo ValorVerificable<Tipo>::obtenerValorAVerificar() {
+    return this->valorAVerificar;
+}
 
 #endif //CHIL_VALORVERIFICABLE_H
 
