4d2680c672fac622318fd07864668ec8ed284ee5
Joaco Moreno <joaquin.moreno@grupoesfera.com.ar>
2022-01-22 21:02:26 -0300

    Lanza la excepción si la acción no se comprueba durante una cantidad de tiempo indicado

--- tests/src/modelo/verificacion/VerificacionTest.cpp
+++ tests/src/modelo/verificacion/VerificacionTest.cpp
@@ -5,6 +5,7 @@
 #include <mocks/PlataformaMock.hpp>
 
 #include <climits>
+#include <modelo/verificacion/excepciones/AccionNoEjecutadaExcepcion.h>
 
 using ::testing::_;
 using ::testing::Return;
@@ -97,23 +98,38 @@ TEST(VerificacionTest, recibeMensajeDescriptivoSilanzaExcepcionEnCasoDeQueLosLit
     }
 }
 
-TEST(VerificacionEnBucleTest, verificaEnModoSondeoUnValorDurante1seg) {
-    auto *plataformaMock = new PlataformaMock();
+PlataformaMock *plataformaMock;
+void dadoQueChilEstaActivo() {
+    plataformaMock = new PlataformaMock();
     NUEVO_CHIL_CON(plataformaMock);
+}
 
+void dadoQueElTiempoAvanzaEnMilisegundos() {
     EXPECT_CALL(*plataformaMock, milisegundos())
             .WillOnce(Return(1L))
             .WillOnce(Return(50L))
             .WillOnce(Return(100L))
             .WillOnce(Return(150L))
             .WillOnce(Return(199L))
-            .WillOnce(Return(220L));
-
+            .WillRepeatedly(Return(220L));
+}
+void dadoQueUnaLecturaRetorna1EnElCuartoIntento() {
     EXPECT_CALL(*plataformaMock, leer(_))
             .WillOnce(Return(0))
             .WillOnce(Return(0))
             .WillOnce(Return(0))
             .WillOnce(Return(1));
+}
+
+void dadoQueLaLecturaSiempreRetorna0() {
+    EXPECT_CALL(*plataformaMock, leer(_)).WillRepeatedly(Return(0));
+}
+
+TEST(VerificacionEnBucleTest, verificaEnModoSondeoUnaAccionDurante200milisegundos)
+{
+    dadoQueChilEstaActivo();
+    dadoQueElTiempoAvanzaEnMilisegundos();
+    dadoQueUnaLecturaRetorna1EnElCuartoIntento();
 
     ASSERT_NO_THROW(
             comprobar([]() {
@@ -122,4 +138,24 @@ TEST(VerificacionEnBucleTest, verificaEnModoSondeoUnValorDurante1seg) {
             ->durante(200)
             ->seHayaEjecutado();
     );
+
+    delete plataformaMock;
+}
+
+TEST(VerificacionEnBucleTest, lanzaExcepcionSiLaAccionNuncaSeComprueba)
+{
+    dadoQueChilEstaActivo();
+    dadoQueElTiempoAvanzaEnMilisegundos();
+    dadoQueLaLecturaSiempreRetorna0();
+
+    ASSERT_THROW(
+            comprobar([]() {
+                return PLATAFORMA->leer(PIN_BOTON) == 1;
+            })
+            ->durante(200)
+            ->seHayaEjecutado(),
+
+            AccionNoEjecutadaExcepcion);
+
+    delete plataformaMock;
 }
