ab824f91429906920bd613029953fcf010eed88a
Joaco Moreno <joaquin.moreno@grupoesfera.com.ar>
2022-01-23 21:38:30 -0300

    Hereda ValoresDistintosExcepcion de ComparacionExcepcion

--- src/modelo/verificacion/excepciones/ValoresDistintosExcepcion.h
+++ src/modelo/verificacion/excepciones/ValoresDistintosExcepcion.h
@@ -2,37 +2,15 @@
 #ifndef CHIL_VALORESDISTINTOSEXCEPCION_H
 #define CHIL_VALORESDISTINTOSEXCEPCION_H
 
-#include <exception>
-#include <string>
-#include <utilidades/literales.h>
-
-using std::exception;
-using std::string;
-using literales::aTexto;
+#include "ComparacionExcepcion.h"
 
 template <typename T>
-class ValoresDistintosExcepcion : public exception {
-
-private:
-    constexpr static const char *const TEXTO_ESPERADO = "Se esperaba el valor: ";
-    constexpr static const char *const TEXTO_RECIBIDO = "Pero se recibio el valor: ";
-    constexpr static const char *const SALTO_DE_LINEA = "\n";
-
-    string mensaje{};
+class ValoresDistintosExcepcion: public ComparacionExcepcion<T> {
 
 public:
-    explicit ValoresDistintosExcepcion(T recibido, T esperado) {
-        mensaje.append(TEXTO_ESPERADO).append(SALTO_DE_LINEA);
-        mensaje.append(aTexto(esperado));
-        mensaje.append(SALTO_DE_LINEA);
-        mensaje.append(TEXTO_RECIBIDO).append(SALTO_DE_LINEA);
-        mensaje.append(aTexto(recibido));
-        mensaje.append(SALTO_DE_LINEA);
-    }
+    explicit ValoresDistintosExcepcion(T recibido, T esperado):
+             ComparacionExcepcion<T>(recibido, esperado, "igual"){}
 
-    string obtenerMensaje() {
-        return mensaje;
-    }
 };
 
 #endif //CHIL_VALORESDISTINTOSEXCEPCION_H
