d917deaadc9aa7fac2c5e33a224279017e64689f
Joaco Moreno <joaquin.moreno@grupoesfera.com.ar>
2022-01-26 19:45:30 -0300

    Agrega prueba para probar la exportacion con 2 escenarios

--- tests/src/modelo/exportador/ExportadorJSONTest.cpp
+++ tests/src/modelo/exportador/ExportadorJSONTest.cpp
@@ -34,9 +34,27 @@ TEST(ExportadorJSON, exportaUnEscenarioConElUnPasoExitosoYUnPasoFallido) {
     delete laPlataforma;
 }
 
+TEST(ExportadorJSON, exportaUnEscenarioDosEscenariosUnoFallidoYOtroExitoso) {
+    dadoQueSePuedeEjecutar4PasosConChil();
+
+    ESCENARIO(Recibe comando en pin 1 para pasar comando al pin 333) {
+        PASO(Recibe comando en el pin 1, []() {});
+    }
+    ESCENARIO(Deja de recibir comando en pin 1 para pasar comando al pin 12) {
+        PASO(No recibe comando en el pin 1, []() { verificar(false)->esVerdadero(); });
+    };
+
+    string escenariosJSON = ExportadorJSON::exportarEscenarios();
+
+    ASSERT_EQ(escenariosJSON, "{\"resumen\":{\"completados\":2,\"exitosos\":1,\"fallidos\":1},\"escenarios\":[{\"nombre\":\"Recibe comando en pin 1 para pasar comando al pin 333\",\"exitoso\":true,\"pasos\":[{\"nombre\":\"Recibe comando en el pin 1\",\"exitoso\":true,\"tiempo\":100,\"detalleDeError\":\"\"}]},{\"nombre\":\"Deja de recibir comando en pin 1 para pasar comando al pin 12\",\"exitoso\":false,\"pasos\":[{\"nombre\":\"No recibe comando en el pin 1\",\"exitoso\":false,\"tiempo\":39,\"detalleDeError\":\"Se esperaba que el valor 0 sea igual a 1 pero no lo fue\"}]}]}");
+
+    delete laPlataforma;
+}
+
+
 void dadoQueSePuedeEjecutar2PasosConChil(){
     laPlataforma = new PlataformaMock();
-
+    EXPECT_CALL(*laPlataforma, consola(_)).Times(1);
     EXPECT_CALL(*laPlataforma, microsegundos())
             .WillOnce(Return(0L))
             .WillOnce(Return(100L));
@@ -47,6 +65,7 @@ void dadoQueSePuedeEjecutar2PasosConChil(){
 void dadoQueSePuedeEjecutar4PasosConChil(){
     laPlataforma = new PlataformaMock();
 
+    EXPECT_CALL(*laPlataforma, consola(_)).Times(1);
     EXPECT_CALL(*laPlataforma, microsegundos())
             .WillOnce(Return(0L))
             .WillOnce(Return(100L))
