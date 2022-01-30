57b1e7701cdbac31d55bc8f50766ce5823a5fba4
Joaco Moreno <joaquin.moreno@grupoesfera.com.ar>
2022-01-30 17:19:53 -0300

    Agrega flag -c para limpiar el contexto antes de ejecutar el build

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,14 +3,18 @@
 set -e
 
 BASEDIR=$(dirname "$0")
+CARPETA="$BASEDIR/../"
 
-
-
-if [[ $1 == "-t" ]]; then
-  cmake $BASEDIR/../ -DBUILD_TESTING:bool=true
-else
-  cmake $BASEDIR/../
+# Si recibe el flag -c hace una limpieza antes
+if [[ $1 == "-c" ]] || [[ $2 == "-c" ]]; then
+  cmake --build "$CARPETA" --target clean
 fi
 
+# Si recibe el flag -t incluye el modulo de pruebas, sino simplemente hace el build
+if [[ $1 == "-t" ]] || [[ $2 == "-t" ]]; then
+  cmake "$CARPETA" -DBUILD_TESTING:bool=true
+else
+  cmake "$CARPETA"
+fi
 
-make -C $BASEDIR/../
+make -C "$CARPETA"
