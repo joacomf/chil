966f6e03a3196dcea46bd31958976befd4d46d91
CD Pipeline <>
2022-02-01 20:35:58 -0300

    Guarda la variable de ambiente PLATFORMIO_AUTH_TOKEN para poder autenticarse a platformio

--- scripts/generar-paquete-platformio.sh
+++ scripts/generar-paquete-platformio.sh
@@ -8,9 +8,6 @@ echo -e "\xE2\x9C\x94 Listo"
 
 echo -e "\e[33mListando directorio\e[39m"
 ls -l
-
-echo -e "\e[33mAutorizando platformio\e[39m"
-pio account token -p "$1"
 echo -e "\xE2\x9C\x94 Listo"
 
 echo -e "\e[33mPublicando paquete\e[39m"
