514a2dda4d06abfa68426fc7e9222d4baf3915fd
CD Pipeline <>
2022-02-01 20:25:52 -0300

    Cambia pasos de pipeline para solucionar problemas al generar version

--- scripts/generar-paquete-platformio.sh
+++ scripts/generar-paquete-platformio.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 echo -e "\e[33mGenerando paquete\e[39m"
 pio package pack .
 echo -e "\xE2\x9C\x94 Listo"
@@ -8,7 +10,7 @@ echo -e "\e[33mListando directorio\e[39m"
 ls -l
 
 echo -e "\e[33mAutorizando platformio\e[39m"
-pio account token -p "$PLATFORMIO_TOKEN"
+pio account token -p "$1"
 echo -e "\xE2\x9C\x94 Listo"
 
 echo -e "\e[33mPublicando paquete\e[39m"
