c2d7bf5a8e6166dde1d596d90d8a1b84babe485d
CD Pipeline <>
2022-05-31 21:46:16 -0300

    Agrega flag para generar el reporte de cobertura

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,13 +3,17 @@
 set -e
 BASEDIR=$(dirname "$0")
 
-if [[ $1 == "-no-build" ]] || [[ $2 == "-no-build" ]]; then
+if [[ $1 == "-no-build" ]] || [[ $2 == "-no-build" ]] || [[ $3 == "-no-build" ]]; then
     "$BASEDIR"/../tests/tests
 else
     "$BASEDIR"/build.sh
     "$BASEDIR"/../tests/tests
 fi
 
-if [[ $1 == "-cobertura" ]] || [[ $2 == "-cobertura" ]]; then
+if [[ $1 == "-cobertura" ]] || [[ $2 == "-cobertura" ]] || [[ $3 == "-cobertura" ]]; then
     make gcov && make lcov
+fi
+
+if [[ $1 == "-reporte" ]] || [[ $2 == "-reporte" ]] || [[ $3 == "-reporte" ]]; then
+    make reporte
 fi
